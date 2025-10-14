6a9bd1390f37ed266d6d07ae11956a268f7928ff
Ustym893 bc20256-2 <uperepelytsia@gmail.com>
2025-10-14 08:50:11 +0300

    created main and other files

--- main.js
+++ main.js
@@ -1,9 +1,57 @@
-const program = require('commander');
+import { Command } from 'commander';
+import fs from 'fs';
 
-program.option('-i, --input').option('-o, --output').option('-d, --display');
+const program = new Command();
 
-program.phrase();
+// Налаштовуємо параметри командного рядка
+program
+  .requiredOption('-i, --input <path>', 'шлях до вхідного файлу JSON')
+  .option('-o, --output <path>', 'шлях до вихідного файлу')
+  .option('-d, --display', 'вивести результат у консоль')
+  .option('-f, --furnished', 'показати лише будинки з меблями (furnished)')
+  .option(
+    '-p, --price <number>',
+    'показати лише будинки з ціною нижче зазначеної'
+  );
 
+program.parse(process.argv);
 const options = program.opts();
-console.log('');
 
+// --- Перевірка наявності файлу ---
+if (!options.input) {
+  console.error('Please, specify input file');
+  process.exit(1);
+}
+
+if (!fs.existsSync(options.input)) {
+  console.error('Cannot find input file');
+  process.exit(1);
+}
+
+// --- Читання JSON ---
+const data = JSON.parse(fs.readFileSync(options.input, 'utf8'));
+
+// --- Фільтрація ---
+let result = data;
+
+if (options.furnished) {
+  result = result.filter((item) => item.furnishingstatus === 'furnished');
+}
+
+if (options.price) {
+  const maxPrice = Number(options.price);
+  result = result.filter((item) => item.price < maxPrice);
+}
+
+// --- Формування виводу ---
+const output = result.map((item) => `${item.price} ${item.area}`).join('\n');
+
+// --- Вивід або запис ---
+if (options.display && options.output) {
+  console.log(output);
+  fs.writeFileSync(options.output, output);
+} else if (options.display) {
+  console.log(output);
+} else if (options.output) {
+  fs.writeFileSync(options.output, output);
+}
