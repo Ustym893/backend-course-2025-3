534fc736e5a20148c7d41d9e54ec1b58644c20c1
Ustym893 bc-2025-4 <uperepelytsia@gmail.com>
2025-10-21 09:53:27 +0300

    modifide main

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ import fs from 'fs';
 
 const program = new Command();
 
-// Налаштовуємо параметри командного рядка
 program
   .requiredOption('-i, --input <path>', 'шлях до вхідного файлу JSON')
   .option('-o, --output <path>', 'шлях до вихідного файлу')
@@ -17,7 +16,6 @@ program
 program.parse(process.argv);
 const options = program.opts();
 
-// --- Перевірка наявності файлу ---
 if (!options.input) {
   console.error('Please, specify input file');
   process.exit(1);
@@ -28,10 +26,8 @@ if (!fs.existsSync(options.input)) {
   process.exit(1);
 }
 
-// --- Читання JSON ---
 const data = JSON.parse(fs.readFileSync(options.input, 'utf8'));
 
-// --- Фільтрація ---
 let result = data;
 
 if (options.furnished) {
@@ -43,10 +39,8 @@ if (options.price) {
   result = result.filter((item) => item.price < maxPrice);
 }
 
-// --- Формування виводу ---
 const output = result.map((item) => `${item.price} ${item.area}`).join('\n');
 
-// --- Вивід або запис ---
 if (options.display && options.output) {
   console.log(output);
   fs.writeFileSync(options.output, output);
